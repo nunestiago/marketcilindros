31c3abf34285194a2c22b9e60d4e419e4038dab8
Tiago Nunes <ntiagon@gmail.com>
2021-08-07 12:43:37 -0300

    add Home and Register

--- front-end/src/components/index.js
+++ front-end/src/components/index.js
@@ -1,1 +1,3 @@
 export { default as Loading } from './Loading/Loading';
+export { default as PasswordInput } from './PasswordInput/PasswordInput';
+export { default as CustomAlert } from './CustomAlert/CustomAlert';
