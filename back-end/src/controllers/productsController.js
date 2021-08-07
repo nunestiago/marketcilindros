e59aa96372387fa8d4b1e66a2ad0d6e8c72c7ddf
Tiago Nunes <ntiagon@gmail.com>
2021-08-07 17:47:01 -0300

    added logic and fixed id  receveid

--- back-end/src/controllers/productsController.js
+++ back-end/src/controllers/productsController.js
@@ -1,17 +1,38 @@
 import connect from '../database/connect';
 
 export const myProducts = async (req, res) => {
-  const { id } = req.body;
+  const { id } = req.user;
+
   try {
-    const query = 'select * from produtos where usuario_id = $1';
+    const query = 'select * from produtos p where p.usuario_id = $1';
     const products = await connect.query(query, [id]);
 
-    res.status(200).json(products);
+    res.status(200).json(products.rows);
   } catch (error) {
     return res.status(400).json(error.message);
   }
 };
 
 export const addProduct = async (req, res) => {
-  res.status(200).json('ok');
+  const { userId, name, price, stock, description, image } = req.body;
+  try {
+    const query =
+      'insert into produtos (usuario_id, nome, estoque, preco,  descricao, imagem) values ($1,$2,$3,$4,$5,$6)';
+    const { rowCount: newProduct } = await connect.query(query, [
+      userId,
+      name,
+      price,
+      stock,
+      description,
+      image,
+    ]);
+    if (!newProduct) {
+      return res
+        .status(400)
+        .json({ error: 'Não foi possível registrar o produto' });
+    }
+    return res.status(200).json('Produto registrado');
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
 };
