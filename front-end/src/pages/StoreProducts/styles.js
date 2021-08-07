c7e521304010be386f078fb7dd5d63f8f0571a4e
Tiago Nunes <ntiagon@gmail.com>
2021-08-07 13:57:39 -0300

    styles store products

---
+++ front-end/src/pages/StoreProducts/styles.js
@@ -0,0 +1,20 @@
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginLeft: 60,
+    marginTop: 78,
+  },
+  title: {
+    marginBottom: 86,
+  },
+  subtitle: {
+    marginBottom: 57,
+  },
+  divider: {
+    margin: '1rem 0',
+    width: '100%',
+  },
+}));
+
+export default useStyles;
