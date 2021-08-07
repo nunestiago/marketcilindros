0238f63c17e03b2e98e63c9229556b657d1c6e5d
Tiago Nunes <ntiagon@gmail.com>
2021-08-07 12:27:40 -0300

    some refactor to loginjs center things

--- front-end/src/pages/Login/Login.js
+++ front-end/src/pages/Login/Login.js
@@ -24,46 +24,54 @@ function Login() {
   return (
     <div className={classes.root}>
       <Loading loading={loading} />
+
       <Paper className={classes.paper} elevation={10}>
         <Grid container spacing={2}>
-          <Typography variant='h4' gutterBottom={true}>
-            Login
-          </Typography>
-          <form className={classes.form} onSubmit={handleSubmit(handleLogin)}>
-            {' '}
-            <TextField
-              label='E-mail'
-              fullWidth
-              autoFocus
-              type='email'
-              error={!!errors.email}
-              {...register('email', {
-                required: 'E-mail obrigatório',
-                pattern: {
-                  value: emailRegex,
-                  message: 'E-mail inválido',
-                },
-              })}
-            />
-            <PasswordInput
-              label={'Senha'}
-              error={!!errors.password}
-              register={() =>
-                register('password', { required: 'Senha obrigatório' })
-              }
-            />
-            <Button type='submit' color='primary' variant='contained'>
-              Entrar
-            </Button>
-            <CustomAlert errors={errors} />
-            <Typography
-              variant='caption'
-              display='block'
-              style={{ width: '100%' }}
-            >
-              Primeira vez aqui? <Link to={'/register'}>CRIE UMA CONTA</Link>
+          <Grid item xs>
+            <Typography variant='h4' gutterBottom={true}>
+              Login
             </Typography>
-          </form>
+
+            <form className={classes.form} onSubmit={handleSubmit(handleLogin)}>
+              <TextField
+                label='E-mail'
+                fullWidth
+                autoFocus
+                type='email'
+                error={!!errors.email}
+                {...register('email', {
+                  required: 'E-mail obrigatório',
+                  pattern: {
+                    value: emailRegex,
+                    message: 'E-mail inválido',
+                  },
+                })}
+              />
+
+              <PasswordInput
+                label={'Senha'}
+                error={!!errors.password}
+                register={() =>
+                  register('password', { required: 'Senha obrigatório' })
+                }
+              />
+
+              <CustomAlert errors={errors} />
+
+              <Button type='submit' color='primary' variant='contained'>
+                Entrar
+              </Button>
+
+              <Typography
+                variant='caption'
+                display='block'
+                style={{ width: '100%' }}
+              >
+                Primeira vez aqui?
+                <Link to={'/register'}>CRIE UMA CONTA</Link>
+              </Typography>
+            </form>
+          </Grid>
         </Grid>
       </Paper>
     </div>
