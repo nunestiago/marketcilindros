9313361249b8ae3c4e27b6e59d98735ee66b39c3
Tiago Nunes <ntiagon@gmail.com>
2021-08-07 12:54:57 -0300

    passed context provider to app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import { CssBaseline, ThemeProvider } from '@material-ui/core';
 
 import theme from './config/theme';
+import { AuthProvider } from './context/AuthContext';
 import Routes from './routes/Routes';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Routes />
+      <AuthProvider>
+        <CssBaseline />
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   );
 }
