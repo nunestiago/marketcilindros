387067b25364e234e103a6e814cccbb32d7f2611
Tiago Nunes <ntiagon@gmail.com>
2021-08-06 21:07:22 -0300

    db connected

---
+++ back-end/src/database/connect.js
@@ -0,0 +1,15 @@
+import { Pool } from 'pg';
+
+require('dotenv').config();
+
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+});
+
+module.exports = {
+  query: (text, params, callback) => pool.query(text, params, callback),
+};
