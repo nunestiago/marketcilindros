e049b32d8f81247ae97075c45f5944e7e6b52032
Tiago Nunes <ntiagon@gmail.com>
2021-08-07 13:32:22 -0300

    updates subroutes with new components

--- front-end/src/components/index.js
+++ front-end/src/components/index.js
@@ -1,3 +1,6 @@
 export { default as Loading } from './Loading/Loading';
 export { default as PasswordInput } from './PasswordInput/PasswordInput';
 export { default as CustomAlert } from './CustomAlert/CustomAlert';
+export { default as CustomCard } from './CustomCard/CustomCard';
+export { default as DeleteDialog } from './DeleteDialog/DeleteDialog';
+export { default as SideBar } from './SideBar/Sidebar';
--- front-end/src/pages/index.js
+++ front-end/src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as Login } from './Login/Login';
 export { default as Register } from './Register/Register';
 export { default as Home } from './Home/Home';
+export { default as StoreProducts } from './StoreProducts/StoreProducts';
