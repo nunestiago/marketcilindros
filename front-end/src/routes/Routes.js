f77f31d0cf0ab8d6ed93dca38b800cfebafb53df
Tiago Nunes <ntiagon@gmail.com>
2021-08-07 12:43:55 -0300

    add basic home for easer access

--- front-end/src/routes/Routes.js
+++ front-end/src/routes/Routes.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import { Login } from '../pages';
+import { Home, Login, Register } from '../pages';
 
 function Routes() {
   return (
     <Router>
       <Switch>
-        <Route path='/' component={Login} />
+        <Route path='/' exact component={Home} />
+        <Route path='/login' component={Login} />
+        <Route path='/register' component={Register} />
       </Switch>
     </Router>
   );
