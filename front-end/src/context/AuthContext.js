57b136a0b44a8927b2e0fb887f94b06ef9bb0627
Tiago Nunes <ntiagon@gmail.com>
2021-08-09 09:43:52 -0300

    increase context

--- front-end/src/context/AuthContext.js
+++ front-end/src/context/AuthContext.js
@@ -5,17 +5,21 @@ export const AuthContext = createContext();
 function AuthProvider({ children }) {
   const [user, setUser] = useState({});
   const [token, setToken] = useState('');
+  const [productTo, setProductTo] = useState({});
 
   return (
-    <AuthContext.Provider value={{ user, setUser, token, setToken }}>
+    <AuthContext.Provider
+      value={{ user, setUser, token, setToken, productTo, setProductTo }}
+    >
       {children}
     </AuthContext.Provider>
   );
 }
 
 function UseAuth() {
-  const { user, setUser, token, setToken } = useContext(AuthContext);
-  return { user, setUser, token, setToken };
+  const { user, setUser, token, setToken, productTo, setProductTo } =
+    useContext(AuthContext);
+  return { user, setUser, token, setToken, productTo, setProductTo };
 }
 
 export { AuthProvider, UseAuth };
